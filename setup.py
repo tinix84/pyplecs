ae60c2940b72d9e0d3ea65ced05aa229d13cad3f
tinix84 <tinix84@gmail.com>
2020-01-25 14:52:20 +0100

    new tests

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='pyplecs',
+    version='',
+    packages=['tests', 'pyplecs'],
+    url='',
+    license='',
+    author='riccardo tinivella',
+    author_email='tinix84@gmail.com',
+    description='automatization of plecs simulations'
+)
